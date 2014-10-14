fdbe310475d271db4a422ac124116f4e44047af3
Gaetano Catalli <gaetano.catalli@gmail.com>
2014-10-15 00:42:08 +0200

    continue prices scraping

--- Scraper.py
+++ Scraper.py
@@ -1,6 +1,7 @@
 import httplib
 import urllib
 from urlparse import urlparse, parse_qs
+import bs4
 from bs4 import BeautifulSoup
 import re
 
@@ -25,10 +26,58 @@ class TrenitaliaParser():
 				opt["depTime"] = depTd.span.string
 				opt["arrTime"] = arrTd.span.string
 				opt["duration"] = durTd.span.string
-				tmp = re.sub(r'[\t\n]*', '', trainTd.div.span.getText()).split()
+				tmp = re.sub(r'[\t\n]*', '',
+					     trainTd.div.span.getText()).split()
 				opt["trainType"] = tmp[0]
 				opt["trainCode"] = tmp[1]
-				opt["price"] = re.sub(r'[\s]', '', priceTd.span.get_text())[:-1]
+				opt["minPrice"] = re.sub(r'[\s]', '',
+						         priceTd.span.get_text())[:-1]
+				opt["prices"] = list()
+				for sib in tr.find_next_siblings("tr"):
+					if sib.has_attr("id") and sib["id"].startswith("sd_"):
+						priceTab = sib.tbody
+						# skip 2 tr
+						priceTr = priceTab.find_next("tr").find_next("tr")
+						for t in priceTr.find_next_siblings("tr"):
+							p = dict()
+							td1 = t.find_next("td")
+							if td1.span and td1.span.string:
+								p["fareType"] = td1.span.string.strip()
+								p["farePrices"] = list()
+
+								pr = td1.find_next_sibling("td")
+								tmp = re.sub(r'[\s]', '', pr.span.get_text())[:-1]
+								p["farePrices"].append({"type" : "STANDARD",
+												 "price": tmp })
+
+								pr = td1.find_next_sibling("td")
+								tmp = re.sub(r'[\s]', '', pr.span.get_text())[:-1]
+								p["farePrices"].append({"type" : "PREMIUM",
+												 "price": tmp })
+
+								pr = td1.find_next_sibling("td")
+								tmp = re.sub(r'[\s]', '', pr.span.get_text())[:-1]
+								p["farePrices"].append({"type" : "BUISNESS SALOTTINO",
+												 "price": tmp })
+
+								pr = td1.find_next_sibling("td")
+								tmp = re.sub(r'[\s]', '', pr.span.get_text())[:-1]
+								p["farePrices"].append({"type" : "BUISNESS AREA SILENZIO",
+												 "price": tmp })
+
+								pr = td1.find_next_sibling("td")
+								tmp = re.sub(r'[\s]', '', pr.span.get_text())[:-1]
+								p["farePrices"].append({"type" : "BUISNESS",
+												 "price": tmp })
+
+								pr = td1.find_next_sibling("td")
+								tmp = re.sub(r'[\s]', '', pr.span.get_text())[:-1]
+								p["farePrices"].append({"type" : "EXECUTIVE",
+												 "price": tmp })
+
+								opt["prices"].append(p)
+						break
+
 				self.result.append(opt)
 		return self.result
 
@@ -74,13 +123,18 @@ if __name__ == "__main__":
 			      "Bologna Centrale",
 			      "18-10-2014",
 			      "15")
-	# f = open("sample.html", "w")
-	# f.write(html)
-	# f.close()
+	f = open("sample.html", "w")
+	f.write(html)
+	f.close()
 
 	parser = TrenitaliaParser(html)
 	result = parser.getResults()
-	f = 'type: {0}({1})\tleave: {2}\tarrive: {3}\tduration: {4}\tprice: {5}'
-	for x in result:
-		print f.format(x['trainType'], x['trainCode'], x['depTime'],
-				x['arrTime'], x['duration'], x['price'])
+	# f = 'type: {0}({1})\tleave: {2}\tarrive: {3}\tduration: {4}\tprice: {5}'
+	# for x in result:
+	# 	print f.format(x['trainType'], x['trainCode'], x['depTime'],
+	# 			x['arrTime'], x['duration'], x['minPrice'])
+	x = result[0]
+	for y in x["prices"]:
+		print("type: " + y["fareType"])
+		for z in y["farePrices"]:
+			print("\ttype: " + z["type"] + "\t price: " + z["price"])
