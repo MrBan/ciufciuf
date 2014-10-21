207412235549038fd16161053e681c9aef8ee32f
Gaetano Catalli <gaetano.catalli@gmail.com>
2014-10-21 22:46:14 +0200

    test: find the first SUPER ECONOMY solution

--- test.py
+++ test.py
@@ -2,29 +2,41 @@
 """Trenitalia test module"""
 
 from tabulate import tabulate
+from datetime import date, timedelta
 
 from Trenitalia import Trenitalia
 
+def daterange(start_date, end_date):
+    for n in range(int ((end_date - start_date).days)):
+        yield start_date + timedelta(n)
+
 if __name__ == "__main__":
-    TP = Trenitalia({"dep": "Firenze S. M. Novella",
-                     "arr": "Milano Centrale",
-                     "date": "21-11-2014",
-                     # "time": "07",
-                     "nAdults": "1",
-                     "code": "9502"})
+    START = date.today()
+    END = START + timedelta(30)
 
-    RES = TP.find_page()
+    for d in daterange(START, END):
+        TP = Trenitalia({"dep": "Firenze S. M. Novella",
+                         "arr": "Milano Centrale",
+                         "date": d.strftime("%d-%m-%Y"),
+                         "time": "07",
+                         "nAdults": "1",
+                         "code": "9502"})
 
-    for train in RES:
-        print "Train Code:\t" + train["trainCode"]
-        print "Departure:\t" + train["depTime"]
-        print "Arrival:\t" + train["arrTime"]
-        print "Duration:\t" + train["duration"]
-        table = list()
-        for pr in train["prices"]:
-            row = [pr["fareType"]]
-            for x in pr["farePrices"]:
-                row.append(x["price"])
-            table.append(row)
-        print
-        print tabulate(table, headers=TP.COMFORTS)
+        RES = TP.find_page()
+        if len(RES) > 0:
+            train = RES[0]
+            table = list()
+            print d.strftime("%A %d %B %Y")
+            for pr in train["prices"]:
+                if pr["fareType"] == "SUPER ECONOMY":
+                    for x in pr["farePrices"]:
+                        if x["price"] != "N/A":
+                            print (d.strftime("%A %d %B %Y") + " " + x["comfort"]+ " " +str(x["price"]))
+                            break
+                    else:
+                        continue
+                    break
+            else:
+                continue
+            break
+    print "The end"
